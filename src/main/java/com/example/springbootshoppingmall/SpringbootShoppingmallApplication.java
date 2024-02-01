76241da7713e0f513ed60f016ea2531a7c6d507a
jcowwk <jcowwk@gmail.com>
2024-02-02 01:20:04 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootshoppingmall/SpringbootShoppingmallApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootshoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShoppingmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShoppingmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootshoppingmall/SpringbootShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShoppingmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
