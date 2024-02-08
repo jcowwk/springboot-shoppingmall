1e164f9cd20e1ec5209ccba8d9d200e5f1c51836
jcowwk <jcowwk@gmail.com>
2024-02-08 13:32:32 +0900

    feat: 게시판 목록 불러오기 및 글 삭제 기능까지 구현

--- src/main/java/com/example/springbootshoppingmall/controller/BoardController.java
+++ src/main/java/com/example/springbootshoppingmall/controller/BoardController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -51,16 +52,30 @@ public class BoardController {
         return "board/boardpage";
     }
 
-    @GetMapping("/updatepage")
-    public String UpdatePage() { return "board/updatepage"; }
-
-    @PostMapping("/update")
-    public String Update() {
-        return "board/readpage";
+    @GetMapping("/updatepage/{board_id}")
+    public String UpdatePage(@PathVariable int board_id, Model model) {
+        BoardEntity boardEntity = boardRepository.findById(board_id).orElse(null);
+        model.addAttribute("board",boardEntity);
+        return "board/updatepage";
     }
 
-    @GetMapping("/delete")
-    public String Delete() {
+    @PostMapping("/update")
+    public String Update(BoardDto boardDto) {
+        BoardEntity boardEntity = boardDto.toEntity();
+        boardRepository.save(boardEntity);
+
+        return "redirect:/board/readpage/" + boardEntity.getBoard_id();
+    }
+
+    @GetMapping("/delete/{board_id}")
+    public String Delete(@PathVariable int board_id, RedirectAttributes rttr) {
+        BoardEntity boardEntity = boardRepository.findById(board_id).orElse(null);
+
+        if (boardEntity != null) {
+            boardRepository.delete(boardEntity);
+            rttr.addAttribute("msg", boardEntity.getBoard_id() + "번 글이 삭제되었습니다.");
+        }
+
         return "board/boardpage";
     }
 }
